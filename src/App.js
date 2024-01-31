11f7b36f73147e68e24070a999699963339c4283
sachin <sachinkumar854881@gmail.com>
2024-01-31 23:28:40 +0530

    add all files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from 'react'
+const styles={
+    color:'red',
+    fontSize:'30px'
 }
-
+function App(){
+    return(
+        <div style={styles}>
+            <h1>Internal Style in React</h1>
+        </div>
+    )
+}
 export default App;
